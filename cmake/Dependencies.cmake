3b512784c29d4256a5c7abc2efa60d397bf6426f
florian.laine <florian.laine@epita.fr>
2024-11-30 15:22:05 +0100

    feat: setup compiling shader

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,7 @@
+find_package(glfw3 REQUIRED)
+find_package(Vulkan REQUIRED)
+find_package(glm REQUIRED)
+find_package(assimp REQUIRED)
+
+find_package(Vulkan COMPONENTS glslc)
+find_program(glslc_executable NAMES glslc HINTS Vulkan::glslc)
